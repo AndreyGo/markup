17457dc0f140b15452d559b554469a0633cc835c
langolf <sasha.langolf@gmail.com>
2014-02-16 03:34:00 +0400

    Правки каталога

--- public/js/app.js
+++ public/js/app.js
@@ -35,13 +35,9 @@ $(document).ready(function() {
 
     // Product item
     $('.product_item').hover(function() {
-      var value = $(this).children('.item_title').text();
-      var link = $('.item_title a', this).attr('href');
-      var price = $(this).children('.item_price').html();
-      $(this).append('<div class="product_item_hover_block"><div class="item_title"><a href="'+ link +'">' + value + '</a></div><div class="item_price">'+ price +'</div><div class="product_item_hover_meta"><a href="#" class="product_item_hover_favorite"><span class="icon icon-product-item-flag"></span><span class="title">В закладки</span></a><button type="submit">Купить</button></div></div>');
-      $('.product_item_hover_block', this).animate({opacity: 1}, 300, "easeInOutCubic");
+      $('.product_item_hover_block', this).animate({opacity: 1}, 200, "easeInOutCubic");
     }, function() {
-      $('.product_item_hover_block').remove();
+      $('.product_item_hover_block', this).animate({opacity: 0}, 200, "easeInOutCubic");
     });
 
     // Item badges
