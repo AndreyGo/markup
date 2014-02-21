ffd54bf3a09b49205a43d96112640b411a8c604c
langolf <sasha.langolf@gmail.com>
2014-02-21 16:53:03 +0400

    Styles for autocomplete

--- public/js/app.js
+++ public/js/app.js
@@ -690,4 +690,39 @@ $(document).ready(function() {
 
         $(this).ajaxSubmit(options);
     });
+    $('input, textarea').focusin(function(){
+      $(this).addClass('page_input-active');
+    });
+    $('input, textarea').focusout(function(){
+      if (!$(this).val() == '') {
+        $(this).css('color', '#000');
+      }
+      $(this).removeClass('page_input-active');
+    });
+
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "Clojure",
+      "C++",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp"
+    ];
+    $('#mainSearch').autocomplete({
+      source: availableTags,
+      appendTo: 'form.search_form',
+      open: function(e, ui) {
+        $('.ui-autocomplete').css('top', $("ul.ui-autocomplete").cssUnit('top')[0] + 10);
+      }
+    });
 });
